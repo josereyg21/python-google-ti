5bf191c8d5d377424932d403145ad7e156357f6b
José Reyes <josereyg21@gmail.com>
2021-08-08 17:43:40 -0600

    update

--- Crash Course on Python/Semana 4/Practice Quiz 3/Exercise 2.py
+++ Crash Course on Python/Semana 4/Practice Quiz 3/Exercise 2.py
@@ -1,10 +1,19 @@
-# The groups_per_user function receives a dictionary, 
-# which contains group names with the list of users. Users can belong to multiple groups. 
-# Fill in the blanks to return a dictionary with the users as keys and a list of their groups as values. 
+def groups_per_user(groups):
+    l = {}
+    s = []
+    v = [x for x in groups]
+    for x in v:
+        s.extend(groups[x])
+    s = set(s)
+    s = list(s)
+    for i in s:
+        for d in groups:
+            if i in groups[d]:
+                l[i] = l.get(i, []) + [d]
+    return l
 
+    
 
-Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
-Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
-
-k = Rorys_guests.keys()
-print(k)
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
