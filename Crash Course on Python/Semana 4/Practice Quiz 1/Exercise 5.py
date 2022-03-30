eca57aa89f92d5265ef887c733006a9521df5c5b
José Reyes <79488817+josereyg21@users.noreply.github.com>
2022-03-29 18:05:55 -0700

    update

--- Crash Course on Python/Semana 4/Practice Quiz 1/Exercise 5.py
+++ Crash Course on Python/Semana 4/Practice Quiz 1/Exercise 5.py
@@ -20,3 +20,24 @@ print(replace_ending("The weather is nice in May", "may", "april"))
 # Should display "The weather is nice in May"
 print(replace_ending("The weather is nice in May", "May", "April")) 
 # Should display "The weather is nice in April"
+
+def replace_ending(sentence, old, new):
+    s1 = sentence.split()
+    if str(s1[-1]) == old:
+        s1.remove(s1[-1])
+        s1.append(new)
+        return " ".join(s1)
+    return sentence
+
+	
+print(replace_ending("It's raining cats and cats", "cats", "dogs")) 
+# Should display "It's raining cats and dogs"
+print(replace_ending("She sells seashells by the seashore", "seashells", "donuts")) 
+# Should display "She sells seashells by the seashore"
+print(replace_ending("The weather is nice in May", "may", "april")) 
+# Should display "The weather is nice in May"
+print(replace_ending("The weather is nice in May", "May", "April")) 
+# Should display "The weather is nice in April"
+
+
+
