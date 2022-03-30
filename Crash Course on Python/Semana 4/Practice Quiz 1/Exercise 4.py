1188f59eee8e8af7ccdab6ac534ce45a17647e72
José Reyes <79488817+josereyg21@users.noreply.github.com>
2022-03-29 17:45:13 -0700

    Update

--- Crash Course on Python/Semana 4/Practice Quiz 1/Exercise 4.py
+++ Crash Course on Python/Semana 4/Practice Quiz 1/Exercise 4.py
@@ -14,3 +14,13 @@ print(nametag("Francesco", "Rinaldi"))
 # Should display "Francesco R." 
 print(nametag("Jean-Luc", "Grand-Pierre")) 
 # Should display "Jean-Luc G." 
+
+def nametag(first_name, last_name):
+	return(f"{first_name}, {last_name[0:1]}.")
+
+print(nametag("Jane", "Smith")) 
+# Should display "Jane S." 
+print(nametag("Francesco", "Rinaldi")) 
+# Should display "Francesco R." 
+print(nametag("Jean-Luc", "Grand-Pierre")) 
+# Should display "Jean-Luc G." 
