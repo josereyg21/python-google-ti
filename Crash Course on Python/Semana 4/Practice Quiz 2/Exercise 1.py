286a03c537a9fa275ddd1bfb32299aed3fea5b28
José Reyes <79488817+josereyg21@users.noreply.github.com>
2022-03-29 23:54:21 -0700

    update

--- Crash Course on Python/Semana 4/Practice Quiz 2/Exercise 1.py
+++ Crash Course on Python/Semana 4/Practice Quiz 2/Exercise 1.py
@@ -7,3 +7,9 @@ newfilenames = [x if not x.endswith('.hpp') else x[:-2] for x in filenames]
 
 print(newfilenames) 
 # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+newfilenames = [x[:-2] if x.endswith("hpp") else x for x in filenames ]
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
